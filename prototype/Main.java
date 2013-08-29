a096a8b54e8d50aec5ec820c4816e5d7c6122acb
Seunghyo Chun <seunghyo.chun@gmail.com>
2013-08-30 00:16:48 +0900

    add prototype pattern

---
+++ prototype/Main.java
@@ -0,0 +1,28 @@
+
+
+import framework.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Manager manager = new Manager();
+		UnderlinePen upen = new UnderlinePen('-');
+
+		MessageBox mbox = new MessageBox('*');
+		MessageBox sbox = new MessageBox('/');
+
+		manager.register("strong message", upen);
+		manager.register("warning box", mbox);
+		manager.register("slash box", sbox);
+
+		// create
+
+		Product p1 = manager.create("strong message");
+		p1.use("Hello, world");
+		Product p2 = manager.create("warning box");
+		p2.use("Hello, world 2");
+		Product p3 = manager.create("slash box");
+		p3.use("Hello, world 3");
+
+
+	}
+}
---
+++ prototype/framework/Manager.java
@@ -0,0 +1,18 @@
+
+package framework;
+import java.util.*;
+
+public class Manager {
+	private HashMap showcase = new HashMap();
+
+	public void register(String name, Product proto) {
+		System.out.println("registered : " + name);
+		showcase.put (name, proto);
+	}
+
+	public Product create(String protoname) {
+		Product p = (Product) showcase.get(protoname);
+		return p.createClone();
+	}
+
+}
