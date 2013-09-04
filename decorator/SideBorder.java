2cf5c99257e433749d0f3cb0ffc1c63002c74acf
Seunghyo Chun <seunghyo.chun@gmail.com>
2013-09-04 22:32:04 +0900

    add decorator pattern

---
+++ decorator/SideBorder.java
@@ -0,0 +1,26 @@
+
+
+public class SideBorder extends Border {
+
+	private char borderChar;
+
+	public SideBorder(Display display, char ch) {
+		super(display);
+		this.borderChar = ch;
+	}
+
+	public int getColumns() {
+		return display.getColumns() + 2;
+	}	
+
+	public int getRows() {
+		return display.getRows();
+	}
+
+	public String getRowText(int row) {
+		return borderChar + display.getRowText(row) + borderChar;
+
+	}
+}
+
+
