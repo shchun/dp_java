e20dc6a37a44f8e3a43655b87e990d5d92465fdd
Seunghyo Chun <seunghyo.chun@gmail.com>
2013-09-12 22:40:42 +0900

    add proxy pattern

---
+++ proxy/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Printable p = new PrinterProxy("Alice");
+		System.out.println("Name is : " + p.getPrinterName());
+		p.setPrinterName("Bob");
+		System.out.println("Name is : " + p.getPrinterName());
+		p.print("Hello, Proxy");
+
+	}
+
+}
